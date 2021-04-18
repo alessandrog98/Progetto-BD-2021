b5e949f659fe90897bbd11ab994e0009e4a7e554
alessandrog98 <878169@stud.unive.it>
2021-04-18 15:47:12 +0200

    test2

--- Site_code/test.py
+++ Site_code/test.py
@@ -1,20 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return render_template('index.html')
 
-@app.route('/users/')
+@app.route('/users')
 def users():
-    return 'if u are a registerd user, please insert the username'
+    return 'if u are a registerd user, please insert the username here: '
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    user = request.form["user"]
+    return redirect(url_for('show_profile', username=user))
 
 @app.route('/users/<username>')
 def show_profile(username):
     users = ['alice','bob','charlie']
     if username in users:
-        return render_template('profile.html', user = username)
+        return render_template('profile.html', user=username)
     else:
-        return render_template('profile.html', reg = users)
+        return render_template('profile.html', reg=users)
 
