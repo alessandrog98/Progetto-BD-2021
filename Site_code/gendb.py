219f7622ec0e7c7e62854ec38a425779598d6d34
alessandro simonato <ale99alealeale@hotmail.it>
2021-04-20 18:24:52 +0200

    update gendb + folder

--- Site_code/gendb.py
+++ Site_code/gendb.py
@@ -3,6 +3,9 @@ from sqlalchemy import *
 import urllib
 from sqlalchemy import create_engine
 import pyodbc as pyodbc
+import datetime
+from sqlalchemy import Column, Integer, DateTime
+
 
 driver = "{ODBC Driver 17 for SQL Server}"
 server = "db-project-2021.database.windows.net"
@@ -22,11 +25,36 @@ metadata = MetaData()
 
 users = Table('Users', metadata, Column('id', Integer, primary_key=True),
                                  Column('email', String),
-                                 Column('pwd', String))
+                                 Column('pwd', String),
+                                 Column('IDGruppo', Integer),
+                                 Column('DataCreazione', TIMESTAMP(timezone=False), nullable=False, default=datetime.datetime.utcnow)
+                                 )
+
+
 
 addresses = Table('addresses', metadata, Column('id', Integer, primary_key=True),
                                          Column('user_id', None, ForeignKey('Users.id')),
-                                         Column('email_address', String, nullable=False))
+                                         Column('email_address', String, nullable=False)
+                                         )
+
+permission= Table('permissions',metadata,
+                                Column('id', Integer, primary_key=True),
+                                 Column('IDGruppo', Integer),
+                                 Column('Tabella', String),
+                                 Column('C', Integer),
+                                 Column('R', Integer),
+                                 Column('U', Integer),
+                                 Column('D', Integer),
+                                 Column('Condizione', String),
+                                 Column('DataCreazione', TIMESTAMP(timezone=False), nullable=False, default=datetime.datetime.utcnow)
+)
+sessions= Table('sessions',metadata,
+                    Column('ID', Integer, primary_key=True),
+                     Column('Token', String),
+                     Column('IDUser', Integer),
+                     Column('IDGruppo', Integer),
+                     Column('DataCreazione', TIMESTAMP(timezone=False), nullable=False, default=datetime.datetime.utcnow)
+              )
 
 metadata.create_all(engine)
 conn = engine.connect()
