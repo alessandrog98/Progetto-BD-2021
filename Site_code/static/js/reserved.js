0a5c4016d34e1cd3a396c7e2f16db000f20355db
Davide Pizzolato <pizzolato.davide00@gmail.com>
2021-06-30 22:46:10 +0200

    Edit reserved

--- Site_code/static/js/reserved.js
+++ Site_code/static/js/reserved.js
@@ -1,11 +1,9 @@
-console.log("caricato")
-
-$(document).ready(function(){
+/*$(function(){
     $(".table").on('click', 'tr', function (e) {
         e.preventDefault();
         var id = $(this).attr('value');
         window.location.href = 'http://127.0.0.1:5000/survey/'+id+'/summary_questions/'
     })
-})
+})*/
 
 
